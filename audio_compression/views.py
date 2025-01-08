94c5d5711c5c6ab91339ce4950b2d41674a84bc9
diopmedoune <dmedoune@ept.sn>
2025-01-08 20:32:13 +0000

    front compression app

--- audio_compression/views.py
+++ audio_compression/views.py
@@ -1,35 +1,48 @@
 from django.shortcuts import render
-from django.core.files.storage import FileSystemStorage
-from django.conf import settings
 import os
-from audio_compression.compression import process_audio, get_compression_info
+from .compression import process_audio, get_compression_info
+from audio_compression.templatetags.custom_filters import bytes_to_mb
 
 def upload_and_compress(request):
+    compression_info = None
     default_frequency = 1000
+    compressed_file_url = None
+    original_file_url = None
 
+    context = {
+        'compression_info': compression_info,
+        'default_frequency': default_frequency,
+        'original_file_url': original_file_url,
+        'compressed_file_url': compressed_file_url,
+    }
+    
     if request.method == 'POST' and request.FILES.get('audio_file'):
         audio_file = request.FILES['audio_file']
-        fs = FileSystemStorage()
-        original_file_path = fs.save(audio_file.name, audio_file)
-        original_file_full_path = fs.path(original_file_path)
+        cutoff_frequency = float(request.POST.get('cutoff_frequency', default_frequency))
 
-        cutoff_frequency = request.POST.get('cutoff_frequency', default_frequency)
-        cutoff_frequency = float(cutoff_frequency)
+        temp_dir = 'media/temp/'
+        if not os.path.exists(temp_dir):
+            os.makedirs(temp_dir)
 
-        compressed_file_name = f"compressed_{audio_file.name}"
-        compressed_file_path = os.path.join(settings.MEDIA_ROOT, compressed_file_name)
+        input_path = os.path.join(temp_dir, audio_file.name)
+        output_path = os.path.join(temp_dir, f"compressed_{audio_file.name}")
 
-        process_audio(original_file_full_path, compressed_file_path, cutoff_frequency)
+        with open(input_path, 'wb+') as temp_file:
+            for chunk in audio_file.chunks():
+                temp_file.write(chunk)
 
-        compression_info = get_compression_info(original_file_full_path, compressed_file_path)
+        process_audio(input_path, output_path, cutoff_frequency)
+        compression_info = get_compression_info(input_path, output_path)
 
-        return render(request, 'audio_compression/compression.html', {
-            'original_file_url': fs.url(original_file_path),
-            'compressed_file_url': fs.url(compressed_file_name),
+        original_file_url = f"/{input_path}"
+        compressed_file_url = f"/{output_path}"
+
+        context = {
             'compression_info': compression_info,
             'default_frequency': default_frequency,
-        })
-
-    return render(request, 'audio_compression/compression.html', {
-        'default_frequency': default_frequency,
-    })
+            'original_file_url': original_file_url,
+            'compressed_file_url': compressed_file_url,
+        }
+        return render(request, 'audio_compression/success_compression.html', context=context)
+    
+    return render(request, 'audio_compression/compression.html', context=context)
